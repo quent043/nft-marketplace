c207effa1adce32d8b64fe01de3ab6c81581299d
Quentin D.C <quent043@hotmail.com>
2022-07-30 13:32:46 +0200

    Odded ownable to NftFactory

--- truffle/contracts/NftFacrtory.sol
+++ truffle/contracts/NftFacrtory.sol
@@ -5,18 +5,19 @@ import "@openzeppelin/contracts/utils/Create2.sol";
 import "./VRFv2Consumer.sol";
 import "./NftCollection.sol";
 import "./MetaVariables.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 //TODO: Ajouter notre email
 //TODO: Events
 
-contract NftFactory is MetaVariables {
+contract NftFactory is MetaVariables, Ownable {
 
     uint64 constant CHAINLINK_SUBSCRIPTION_ID = 9366;
     VRFv2Consumer chanlinkRandomGanarator = new VRFv2Consumer(CHAINLINK_SUBSCRIPTION_ID);
 
     event CollectionDeployed(address contractAddress);
 
-    function createNftCollection(string calldata _uri, uint _max_mint_allowed, uint _max_supply, nftCollectionData[] calldata nftFactoryInputData, uint _amountOfNft) external {
+    function createNftCollection(string calldata _uri, uint _max_mint_allowed, uint _max_supply, nftCollectionData[] calldata nftFactoryInputData, uint _amountOfNft) external onlyOwner {
         // Import the bytecode of the contract to deploy
         bytes memory nftCollectionBytecode = type(NftCollection).creationCode;
         // Make a random salt based on the artist name
