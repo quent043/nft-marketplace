062f3f35bf50614b04627a4c761adb8a1996e5d8
Renaud <vidalrenaud7@gmail.com>
2022-08-03 17:49:19 +0400

    creation de l'upload de NFT

--- client/src/components/uploadToIpfs.jsx
+++ client/src/components/uploadToIpfs.jsx
@@ -3,37 +3,61 @@ import { create } from "ipfs-http-client";
 
 const client = create("https://ipfs.infura.io:5001/api/v0");
 
+
 const Upload = () => {
 
-    const [file, setFile] = useState();
+    
     const [urlArr, setUrlArr] = useState([]);
-  
+
+    const initialFormData = Object.freeze({
+        name: "",
+        description: "",
+        price : 0,
+        file : {}
+      });
+
+    const [formData, updateFormData] = useState(initialFormData);
+
+    const handleChange = (e) => {
+        updateFormData({
+          ...formData,
+          [e.target.name]: e.target.value.trim()
+        });
+      };
+
     const retrieveFile = (e) => {
+      e.preventDefault();
       const data = e.target.files[0];
       const reader = new window.FileReader();
       reader.readAsArrayBuffer(data);
-  
       reader.onloadend = () => {
-        setFile(Buffer(reader.result));
-      };
-  
-      e.preventDefault();
+        updateFormData({
+            ...formData,
+            [e.target.file]: Buffer(reader.result)     
+           
+            });
+        }
     };
 
-
     const handleSubmit = async (e) => {
-      e.preventDefault();
-  
-      try {
 
-        const created = await client.add(file);
-        console.log(created);
+    e.preventDefault();
+
+    const {name, description, price } = formData;
+
+    if (!name || !description || !price || !urlArr) return;
+
+    const data = JSON.stringify({
+        name, description, image : urlArr    
+    })
+
+      try {
+        const created = await client.add(data);
         const url = `https://ipfs.infura.io/ipfs/${created.path}`;
         console.log(url);
-
         setUrlArr((prev) => [...prev, url]);
-
         console.log(urlArr);
+        console.log(data);
 
       } catch (error) {
         console.log("erreur pendant l'upload");
@@ -44,8 +68,11 @@ const Upload = () => {
     return (
       <div className="App">  
         <div className="main">
-          <form onSubmit={handleSubmit}>
-            <input type="file" onChange={retrieveFile} multiple accept='image/png , image/jpg' />
+          <form  className="container mt-4" onSubmit={handleSubmit}>
+            <input type="text" className="form-control" onChange={handleChange} required  name="name" id="name" placeholder="name" />
+            <textarea name="description" id="description" onChange={handleChange} className="form-control" required  cols="20" rows="10"></textarea>
+            <input type="file" onChange={retrieveFile} required className="form-control"  />
+            <input type="text" className="form-control" required onChange={handleChange}  name="price" id="price" placeholder="price" />
             <button type="submit" className="button">Submit</button>
           </form>
         </div>
--- client/src/components/uploadToIpfs_mul.jsx
+++ client/src/components/uploadToIpfs_mul.jsx
@@ -1,36 +1,140 @@
-// MultipleFilesUpload Component : Uploads Multiple files to IPFS and returns the URL
-import React, { useState, useEffect } from 'react'
-import { MultipleFilesUpload } from 'react-ipfs-uploader'
+import React, { useEffect, useRef, useState } from 'react'
+import { FolderUpload } from 'react-ipfs-uploader'
+import { create } from "ipfs-http-client";
+const client = create("https://ipfs.infura.io:5001/api/v0");
+
 
 const UploadMultiple = () => {
-    const [multipleFilesUrl, setMultipleFilesUrl] = useState('')
-    const [files, setFiles] = useState();
 
-      const retrieveFile = (e) => {
-      const data = e.target.files[0];
-      const reader = new window.FileReader();
-      reader.readAsArrayBuffer(data);
-  
-      reader.onloadend = () => {
-        setFiles(Buffer(reader.result));
-      };
-      JSON.stringify(files);
-      e.preventDefault();
-    };
-    
+    //push metadata to ipfs
+    //recover images and urllink
+    //display contrat collections
+    //faire les composant de la pages de création d'un NFT
+    //input (description, metadata ect..)
+    //ajouter un ramdom via chainlink
+
+    //folder URL
+    const [folderUrl, setFolderUrl] = useState('');
+
+    const formData = useRef();
+    const counterRef = useRef(0);
+    const [submitted, setSubmitted] = useState([]);
+    const [smartContratInput, setsmartContratInput] = useState([]);
+
+    //ajouter lien du folder + tokenId + typefichier
+    //ajouter la fonction de mint
+    // ajouter un button de request ramdoness
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        //increase en currentRef
+        counterRef.current += 1;
+
+        //get the first input element from the form
+        const formInputs = [...formData.current.elements].filter(
+            element => element.type === "text" || "number"
+        );
+        //combine the value of the last input with the newcounter ref
+        const newSubmitted = formInputs.reduce(
+            (acc, input) => {
+                return {
+                    ...acc,
+                    [input.name]: input.value
+                };
+            },
+            { number: counterRef.current }
+        );
+
+        //set the NFT value for card
+        setSubmitted(prevSubmitted => [...prevSubmitted, newSubmitted]);
+
+        //converted to the object to tuple to input smartcontrat
+        const result = Object.keys(newSubmitted).map((key) => newSubmitted[key]);
+        setsmartContratInput(prevSubmitted => [...prevSubmitted, result])
+    }
+
     return (
-        <div>
-            <MultipleFilesUpload onChange={retrieveFile} setUrl={setMultipleFilesUrl} />
-            MultipleFilesUrl : <a
-                href={multipleFilesUrl}
-                target='_blank'
-                rel='noopener noreferrer'
-            >
-                {multipleFilesUrl}
-            </a>
+        <>
+            {JSON.stringify(smartContratInput)}
+            <div className="container card-light">
 
-        </div>
+                <div className="main">
+
+                    <FolderUpload setUrl={setFolderUrl} />
+                    URL of the folder on IPSF : <a
+                        href={folderUrl}
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        {folderUrl}
+                    </a>
+
+                    <form ref={formData} className="container mt-4 form-control" onSubmit={handleSubmit}>
+                        <div className="form-group">
+                            <label htmlFor="name-input">NFT's Name</label>
+                            <input type="text" className="form-control" id="name-input" name="name" placeholder="name" required />
+                            <small id="nameHelp" className="form-text text-muted">Give a nice name to your NFT!</small>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="description-input">Description</label>
+                            <textarea type="text" className="form-control" id="description-input" name="description" placeholder="description" />
+                            <small id="nameHelp" className="form-text text-muted">Add an attractive description!</small>
+                        </div>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <label class="input-group-text" for="inputGroupSelect01">Options files types</label>
+                            </div>
+                    
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="price-input">Price (ETH)</label>
+                            <input type="number" className="form-control" id="price-input" name="price" placeholder="price" />
+                            <small id="nameHelp" className="form-text text-muted">Tell us how much it worth!</small>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="royalties-input">Royalties (%)</label>
+                            <input type="number" className="form-control" id="royalties-input" name="royalties" placeholder="royalties in %" />
+                            <small id="nameHelp" className="form-text text-muted">Tell us how much you want to be pay each sell!</small>
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="image-input">Image </label>
+                            <input type="text" value={folderUrl} className="form-control" id="image-input" name="image" placeholder="image URL" disabled />
+                        </div>
+                        <button type="submit" className="btn btn-primary">SUBMIT</button>
+                    </form>
+                    <div>
+                        <h3 id="list-title">Registered NFT</h3>
+
+                        <ol aria-labelledby="list-title">
+                            {submitted.map((input, i) => (
+                                <div className="card">
+
+                                    <div className="card-body">
+                                        <h5 className="card-title">Card title</h5>
+                                        <li key={`index${i}`}>
+                                            <ul>
+                                                <li>TokenId: {input.number}</li>
+                                                <li>name: {input.name} </li>
+                                                <li>description: {input.description}</li>
+                                                <li>price: {input.price} </li>
+                                                <li>royalties: {input.royalties}</li>
+                                                <img className="img-fluid" src={input.image + "/" + input.number + ".png"} alt="Card image cap" />
+                                                <li>Link NFT: {input.image}/{input.number}.png</li>
+                                            </ul>
+                                        </li>
+                                        <a href="#" className="btn btn-secondary">Check it out</a>
+                                    </div>
+                                </div>
+                            ))}
+
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </>
+
     )
 }
 
-export default UploadMultiple
+export default UploadMultiple;
