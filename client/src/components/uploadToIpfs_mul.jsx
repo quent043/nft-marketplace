6ed21ba97f4d67f641a058cdcb2ab1830d57e2f8
Renaud <vidalrenaud7@gmail.com>
2022-08-04 23:56:38 +0400

    modif

--- client/src/components/uploadToIpfs_mul.jsx
+++ client/src/components/uploadToIpfs_mul.jsx
@@ -8,7 +8,7 @@ const client = create("https://ipfs.infura.io:5001/api/v0");
 
 
 const UploadMultiple = () => {
-    const { state: { accounts } } = useEth();
+    const { state: { accounts, nftFactoryContract } } = useEth();
     const [folderUrl, setFolderUrl] = useState('');
     const formData = useRef();
     const counterRef = useRef(0);
@@ -41,7 +41,7 @@ const UploadMultiple = () => {
                 };
             },
             {
-                //number: counter,
+                number: counter,
                 linkNFT: folderUrl + "/" + counter + files
             }
         );
@@ -110,22 +110,22 @@ const UploadMultiple = () => {
                     <form className="container mt-4 form-control" onSubmit={collectionPropsHandler}>
                         <div className="form-group bg-light">
                             <label htmlFor="name-input">Collection Name</label>
-                            <input type="text" ref={NameCollection} className="form-control" id="collection_name-input" name="collectionname" placeholder="collection name" />
+                            <input type="text" ref={NameCollection} className="form-control" id="collection_name-input" name="collectionname" placeholder="collection name" required/>
                             <small id="nameHelp" className="form-text text-muted">Give a nice name to your collection!</small>
                         </div>
                         <div className="form-group bg-light">
                             <label htmlFor="name-input">Royalties %</label>
-                            <input ref={RoyaltieCollection} className="form-control" id="royalties-input" name="royalties" placeholder="collection royalties" />
+                            <input ref={RoyaltieCollection} className="form-control" id="royalties-input" name="royalties" placeholder="collection royalties" required />
                             <small id="nameHelp" className="form-text text-muted">Give the royalties % for your collection!</small>
                         </div>
                         <div className="form-group bg-light">
                             <label htmlFor="name-input">Max supply</label>
-                            <input type="text" ref={maxsupply} className="form-control" id="maxsupply-input" name="royalties" placeholder="maxsupply" />
+                            <input type="text" ref={maxsupply} className="form-control" id="maxsupply-input" name="royalties" placeholder="maxsupply" required />
                             <small id="nameHelp" className="form-text text-muted">Give the maxsupply % for your collection!</small>
                         </div>
                         <div className="form-group bg-light">
                             <label htmlFor="name-input">Max Mint allowed per user</label>
-                            <input type="text" ref={maxmint} className="form-control" id="maxmint-input" name="maxmint" placeholder="maxmint per user" />
+                            <input type="text" ref={maxmint} className="form-control" id="maxmint-input" name="maxmint" placeholder="maxmint per user" required />
                             <small id="nameHelp" className="form-text text-muted">Give the maxsupply % for your collection!</small>
                         </div>
                         <div className="form-group bg-light">
@@ -142,25 +142,25 @@ const UploadMultiple = () => {
 
                         <div className="form-group">
                             <label htmlFor="name-input">NFT's Name</label>
-                            <input type="text" className="form-control" id="name-input" name="name" placeholder="name" />
+                            <input type="text" className="form-control" id="name-input" name="name" placeholder="name" required />
                             <small id="nameHelp" className="form-text text-muted">Give a nice name to your NFT!</small>
                         </div>
 
                         <div className="form-group">
                             <label htmlFor="description-input">Description</label>
-                            <input type="text" className="form-control" id="description-input" name="description" placeholder="description" />
+                            <input type="text" className="form-control" id="description-input" name="description" placeholder="description" required />
                             <small id="nameHelp" className="form-text text-muted">Add an attractive description!</small>
                         </div>
 
                         <div className="form-group">
                             <label htmlFor="price-input">Price (ETH)</label>
-                            <input type="text" className="form-control" id="price-input" name="price" placeholder="price" />
+                            <input type="text" className="form-control" id="price-input" name="price" placeholder="price" required />
                             <small id="nameHelp" className="form-text text-muted">Tell us how much it worth!</small>
                         </div>
 
                         <div className="form-group">
                             <label htmlFor="price-input">Royalties %</label>
-                            <input type="text" value={ColProps.ColRoyalties} disabled className="form-control" id="royalties-input" name="royalties" placeholder="royalties" />
+                            <input type="text" value={ColProps.ColRoyalties} disabled className="form-control" id="royalties-input" name="royalties" placeholder="royalties" required />
                             <small id="nameHelp" className="form-text text-muted">Royalties % of collection %</small>
                         </div>
 
@@ -180,7 +180,7 @@ const UploadMultiple = () => {
                             <div className="card-body">
                                 <h5 className="card-title">{ColProps.collectionName}</h5>
                                     <ul key={`index${i}`} className="list-group">
-                                        <li className="list-group-item" >TokenId: {counter}</li>
+                                        <li className="list-group-item" >TokenId: {input.number}</li>
                                         <li className="list-group-item">Name: {input.name} </li>
                                         <li className="list-group-item">Description: {input.description}</li>
                                         <li className="list-group-item">Price: {input.price} </li>
