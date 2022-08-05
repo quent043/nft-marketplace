caae2c17138474d0e2f36252b335c2045fae7dbd
GenesisBlock2140 <combnathan02@gmail.com>
2022-08-05 18:35:12 +0200

    Adding Footer Components + css

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return(
+        <footer>
+            <p>Developed by KeyMaker using lots of coffee ☕ </p>
+            <a href='https://github.com/quent043/ntf-marketplace'>Github Repository</a>
+        </footer>
+    )
+}
+
+export default Footer;
