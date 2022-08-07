6d0c368f06aeea9a1de7f07e4845a95b1d0fa685
Renaud <vidalrenaud7@gmail.com>
2022-08-07 20:00:52 +0400

    market place and ipfs upload

--- client/src/components/Marketplace.jsx
+++ client/src/components/Marketplace.jsx
@@ -1,18 +1,88 @@
-import React, {useState, useEffect} from 'react';
-
-import { useParams } from 'react-router-dom';
+import React, { Fragment, useState, useEffect } from 'react';
+import { useParams, useLocation, withRouter } from 'react-router-dom';
+import useEth from "../contexts/EthContext/useEth";
+import CardNft from './ui/CardNft';
 
 const Marketplace = () => {
 
-    let {account} = useParams();
-   
+    const { state: { web3, accounts, marketplaceContract, nftCollectionAbi } } = useEth();
+    const { contractAddress, tokenAddress: tokenId } = useParams();
+    const { pathname } = useLocation();
+
+
+    useEffect(() => {
+        getMarketplaceContractItems();
+    }, [web3, nftCollectionAbi, marketplaceContract]);
+
+
+    const [ItemData, setItemData] = useState();
+
+    const [imageURL, setImageURL] = useState();
+    const getMarketplaceContractItems = async () => {
+
+        console.log("marketplaceContract", marketplaceContract);
+        console.log("nftCollectionAbi", nftCollectionAbi);
+        console.log("web3", web3);
+
+        if (marketplaceContract && web3 && nftCollectionAbi) {
+            //tableau des elements de la marketplace
+            const marketplaceItems = [];
+            const imagesUrl = [];
+            //recupere le nombre de items count
+            const itemCount = await marketplaceContract.methods.itemCount().call({ from: accounts[0] });
+
+            console.log("itemCount", itemCount)
+
+            for (let i = 1; i <= parseInt(itemCount, 10); i++) {
+                console.log("boucle")
+                const itemsDetail = await marketplaceContract.methods.itemIdToItemData(i).call();
+
+                console.log("itemsDetail", itemsDetail)
+
+                if (!itemsDetail[4]) {
+
+                    const collection_addr = itemsDetail.nft;
+                    console.log("collection_addr", collection_addr)
+                    const collection_token_id = itemsDetail.tokenId;
+                    const NftContractInstance = new web3.eth.Contract(nftCollectionAbi, collection_addr);
+                    const getNftData = await NftContractInstance.methods.tokenIdToNftData(collection_token_id).call();
+
+                    imagesUrl.push(getNftData.linkToImage)
+                    console.log(imagesUrl)
+                    
+                    marketplaceItems.push(itemsDetail);
+
+                }
+
+            };
+            setImageURL(imagesUrl);
+            setItemData(marketplaceItems);
+
+        }
+    }
+
     return (
-        <div>
-            <p>Exemple de récupération depuis l'addresse HTML {account} via  useParams() </p>
-            <p>Marketplace</p>
-        </div>
+        <>
+            <Fragment>
+            
+         
+                <h2 className='sell--nft--title'> Buy  Art <i>in One click.</i></h2>
+                <div className="container">
+         
+                    {ItemData ?
+                        (ItemData.map((item, index) => (
+                            <div key={index} className="justify-content-center inline-flex">
+                                <CardNft itemCountId={item.marketplaceItemId} marketplace nftId={item.tokenId}  price={item.price} nftImageUrl={imageURL[index]} />   
+                            
+                                </div>
+                        )))
+                        : null}
+                    </div>
+            </Fragment>
+
+        </>
     );
-};
+}
 
 
 export default Marketplace;
--- client/src/components/uploadToIpfs_mul.jsx
+++ client/src/components/uploadToIpfs_mul.jsx
@@ -14,11 +14,11 @@ const UploadMultiple = () => {
     const counterRef = useRef(0);
     const [submitted, setSubmitted] = useState([]);
     const [smartContratInput, setsmartContratInput] = useState([]);
-    const NameCollection = useRef({ collectionName: "null" });
-    const RoyaltieCollection = useRef({ ColRoyalties: "0" });
+    const NameCollection = useRef();
+    const RoyaltieCollection = useRef({ ColRoyalties: 0 });
     const maxmint = useRef("1");
-    const maxsupply = useRef("0");
-    const [ColProps, setColProps] = useState({ collectionName: "null", ColRoyalties: "0", maxsupply: "0", MaxMintAllowed: "1" })
+    const maxsupply = useRef(0);
+    const [ColProps, setColProps] = useState({ collectionName: "null", colRoyalties: 0, maxsupply: 0, MaxMintAllowed: 1 })
     const [page, setpage] = useState(false);
     let counter;
     const [files, setFiles] = useState("");
@@ -26,39 +26,46 @@ const UploadMultiple = () => {
     const handleSubmit = event => {
         event.preventDefault();
         //increase en currentRef
-        counter = counterRef.current += 1;
-
+        counterRef.current += 1;
+       
         //get the first input element from the form
         const formInputs = [...formData.current.elements].filter(
-            element => (element.type === "text" || element.type === "number" || element.type !== "submit"
-            ));
+            element => (element.type === "text" || element.type === "number"));
         //combine the value of the last input with the newcounter ref
         const newSubmitted = formInputs.reduce(
             (acc, input) => {
                 return {
                     ...acc,
-                    [input.name]: input.value
+                    [input.name]: input.value,
                 };
             },
             {
-                linkNFT: folderUrl + "/" + counter + files
+                linkNFT: folderUrl + "/" + counterRef.current + files
             }
         );
-        //set the NFT value for card
+
         setSubmitted(prevSubmitted => [...prevSubmitted, newSubmitted]);
         //converted to the object to tuple to input smartcontrat
-        const result = Object.keys(newSubmitted).map((key) => newSubmitted[key]);
-        setsmartContratInput(prevSubmitted => [...prevSubmitted, result])
+
+        const result = Object.keys(newSubmitted).map((key, i) => (i == 3 ? parseInt(newSubmitted[key], 10) : newSubmitted[key]));
+        const resulttoInt = Object.keys(result).map((key, i) => (i == 4 ? parseInt(result[key], 10) : result[key]));
+
+        setsmartContratInput(prevSubmitted => [...prevSubmitted, resulttoInt])
+
+        console.log(maxsupply)
+        console.log(counterRef.current)
+
     }
 
     const collectionPropsHandler = function (e) {
         e.preventDefault();
         setColProps({
             collectionName: NameCollection.current.value,
-            ColRoyalties: RoyaltieCollection.current.value,
+            colRoyalties: RoyaltieCollection.current.value,
             maxsupply: maxsupply.current.value,
             MaxMintAllowed: maxmint.current.value
         })
+
         setpage(true);
     }
 
@@ -72,127 +79,110 @@ const UploadMultiple = () => {
          * */
 
     const _createNftCollection = async function () {
-        const _maxmint = ColProps.MaxMintAllowed;
-        const _maxsupp = ColProps.maxsupply;
+        const _maxmint = parseInt(ColProps.MaxMintAllowed, 10);
+        const _maxsupp = parseInt(ColProps.maxsupply, 10);
         const _namecoll = ColProps.collectionName;
-  
+
         try {
             /**
              * @parameters string calldata _uri, uint80 _max_mint_allowed, uint80 _max_supply, nftCollectionData[] calldata _nftFactoryInputData
             */
             let response = await nftFactoryContract.methods.createNftCollection(_namecoll, _maxmint, _maxsupp, smartContratInput).send({ from: accounts[0] });
-            console.log("response : ");
-            console.log(response);
+
         } catch (err) {
             //TODO: Gérer les erreurs
             console.log("Error: ", err);
         }
-
-        console.log("Collection Name",_namecoll)
-        console.log("MaxMint",_maxmint)
-        console.log("MaxSupp",_maxsupp)
-        console.log("Imput",smartContratInput)
     }
 
     return (
+
         <>
-        {JSON.stringify(smartContratInput)}
-            <div className="container card-light">
-                <div className="main">
-                    <FolderUpload setUrl={setFolderUrl} />
-                    URL of the folder on IPSF : <Alert>
-                        <a
-                            href={folderUrl}
-                            target='_blank'
-                            rel='noopener noreferrer'
-                        >
-                            {folderUrl}
-                        </a> </Alert>
 
-                    <form className="container mt-4 form-control" onSubmit={collectionPropsHandler}>
-                        <div className="form-group bg-light">
-                            <label htmlFor="name-input">Collection Name</label>
-                            <input type="text" ref={NameCollection} className="form-control" id="collection_name-input" name="collectionname" placeholder="collection name" required/>
-                            <small id="nameHelp" className="form-text text-muted">Give a nice name to your collection!</small>
-                        </div>
-                        <div className="form-group bg-light">
-                            <label htmlFor="name-input">Royalties %</label>
-                            <input ref={RoyaltieCollection} className="form-control" id="royalties-input" name="royalties" placeholder="collection royalties" required />
-                            <small id="nameHelp" className="form-text text-muted">Give the royalties % for your collection!</small>
-                        </div>
-                        <div className="form-group bg-light">
-                            <label htmlFor="name-input">Max supply</label>
-                            <input type="text" ref={maxsupply} className="form-control" id="maxsupply-input" name="royalties" placeholder="maxsupply" required />
-                            <small id="nameHelp" className="form-text text-muted">Give the maxsupply % for your collection!</small>
-                        </div>
-                        <div className="form-group bg-light">
-                            <label htmlFor="name-input">Max Mint allowed per user</label>
-                            <input type="text" ref={maxmint} className="form-control" id="maxmint-input" name="maxmint" placeholder="maxmint per user" required />
-                            <small id="nameHelp" className="form-text text-muted">Give the maxsupply % for your collection!</small>
-                        </div>
-                        <div className="form-group bg-light">
-                            <Button onClick={() => setFiles('.jpeg')} variant="outline-success">jpeg</Button>{' '}
-                            <Button onClick={() => setFiles('.jpg')} variant="outline-warning">jpg</Button>{' '}
-                            <Button onClick={() => setFiles('.png')} variant="outline-danger">png</Button>{' '}
-                            <Button onClick={() => setFiles('.gif')} variant="outline-info">gif</Button>{' '}
-                            <small id="nameHelp" className="form-text text-muted">Give the maxsupply % for your collection!</small>
-                        </div>
-                        <button type="submit" className="btn btn-primary">SUBMIT</button>
-                    </form>
-                    <br></br>
-                    {page === true ? <form ref={formData} className="form-control" onSubmit={handleSubmit}>
+<h2 className='sell--nft--title'> Upload your file to decentralized storage  <i> IPFS</i></h2>
+            <div className="sell--nft--box">
+                <div className="sell--nft--price--box"> 
+                <FolderUpload setUrl={setFolderUrl} />
 
-                        <div className="form-group">
-                            <label htmlFor="name-input">NFT's Name</label>
-                            <input type="text" className="form-control" id="name-input" name="name" placeholder="name" required />
-                            <small id="nameHelp" className="form-text text-muted">Give a nice name to your NFT!</small>
-                        </div>
+                <form className="container mt-4 form-control" onSubmit={collectionPropsHandler}>
+                    <div className="form-group bg-light">
+                        <label htmlFor="name-input">Collection Name</label>
+                        <input type="text" ref={NameCollection} className="form-control" id="collection_name-input" name="collectionname" placeholder="collection name" required />
+                        <small id="nameHelp" className="form-text text-muted">Give a nice name to your collection!</small>
+                    </div>
 
-                        <div className="form-group">
-                            <label htmlFor="description-input">Description</label>
-                            <input type="text" className="form-control" id="description-input" name="description" placeholder="description" required />
-                            <small id="nameHelp" className="form-text text-muted">Add an attractive description!</small>
-                        </div>
+                    <div className="form-group bg-light">
+                        <label htmlFor="name-input">Max supply</label>
+                        <input type="number" ref={maxsupply} className="form-control" id="maxsupply-input" name="maxsupply" placeholder="maxsupply" required />
+                        <small id="nameHelp" className="form-text text-muted">Give the maxsupply % for your collection!</small>
+                    </div>
+                    <div className="form-group bg-light">
+                        <label htmlFor="name-input">Max Mint allowed per user</label>
+                        <input type="number" ref={maxmint} className="form-control" id="maxmint-input" name="maxmint" placeholder="maxmint per user" required />
+                        <small id="nameHelp" className="form-text text-muted">Give the maxsupply % for your collection!</small>
+                    </div>
+                    <div className="form-group bg-light">
+                        <Button onClick={() => setFiles('.jpeg')} variant="outline-success">jpeg</Button>{' '}
+                        <Button onClick={() => setFiles('.jpg')} variant="outline-warning">jpg</Button>{' '}
+                        <Button onClick={() => setFiles('.png')} variant="outline-danger">png</Button>{' '}
+                        <Button onClick={() => setFiles('.gif')} variant="outline-info">gif</Button>{' '}
+                        <small id="nameHelp" className="form-text text-muted">Give the maxsupply % for your collection!</small>
+                    </div>
+                    <button type="submit" className="btn btn-primary">SUBMIT</button>
+                </form>
 
-                        <div className="form-group">
-                            <label htmlFor="price-input">Price (ETH)</label>
-                            <input type="text" className="form-control" id="price-input" name="price" placeholder="price" required />
-                            <small id="nameHelp" className="form-text text-muted">Tell us how much it worth!</small>
-                        </div>
 
-                        <div className="form-group">
-                            <label htmlFor="price-input">Royalties %</label>
-                            <input type="text" value={ColProps.ColRoyalties} disabled className="form-control" id="royalties-input" name="royalties" placeholder="royalties" required />
-                            <small id="nameHelp" className="form-text text-muted">Royalties % of collection %</small>
-                        </div>
+                {page === true ? <form ref={formData} className="form-control" onSubmit={handleSubmit}>
 
-                        <div className="btn-group">
-                            {ColProps.maxsupply == counterRef.current ? <Button onClick={() => { _createNftCollection() }}> Create NFT Collection </Button> : <button type="submit" className="btn btn-primary">SUBMIT</button>}
-                        </div>
-                      
-                    </form>
-                    
-                        : null}
-                </div>
+                    <div className="form-group">
+                        <label htmlFor="name-input">NFT's Name</label>
+                        <input type="text" className="form-control" id="name-input" name="name" placeholder="name" required />
+                        <small id="nameHelp" className="form-text text-muted">Give a nice name to your NFT!</small>
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="description-input">Description</label>
+                        <input type="text" className="form-control" id="description-input" name="description" placeholder="description" required />
+                        <small id="nameHelp" className="form-text text-muted">Add an attractive description!</small>
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="price-input">Price (ETH)</label>
+                        <input type="number" className="form-control" id="price-input" name="price" placeholder="price" required />
+                        <small id="nameHelp" className="form-text text-muted">Tell us how much it worth!</small>
+                    </div>
+                    <div className="form-group bg-light">
+                        <label htmlFor="name-input">Royalties %</label>
+                        <input type="number" className="form-control" id="royalties-input" name="royalties" placeholder="collection royalties" required />
+                        <small id="nameHelp" className="form-text text-muted">Give the royalties % for your collection!</small>
+                    </div>
+
+                    <div className="btn-group">
+                        {ColProps.maxsupply == counterRef.current ? <button type="submit" className="card--nft--btn" onClick={() => { _createNftCollection() }}> Create NFT Collection </button> : <button className="card--nft--btn" type="submit" >SUBMIT</button>}
+                    </div>
+                </form>
+
+                    : null}
             </div>
-          
-                <div className="d-flex">
-                    {submitted.map((input, i) => (
-                        <div className="card bg-light text-dark border-grey">
-                           <img className="w-50 p-4" src={input.linkNFT} />
-                            <div className="card-body">
-                                <h5 className="card-title">{ColProps.collectionName}</h5>
-                                    <ul key={`index${i}`} className="list-group">
-                                        <li className="list-group-item" >TokenId: {counterRef.current}</li>
-                                        <li className="list-group-item">Name: {input.name} </li>
-                                        <li className="list-group-item">Description: {input.description}</li>
-                                        <li className="list-group-item">Price: {input.price} </li>
-                                        <li className="list-group-item">Royalties: {input.royalties}</li>
-                                    </ul>
-                            </div>
-                        </div>
-                    ))}
-              </div>
+            </div>
+        
+        <div className="container">
+
+        {submitted.map((input, i) => (
+                <div className="card--nft--background inline-flex">
+                <img className="card--nft--img" src={input.linkNFT} />
+                    <h5 className="card-title">{ColProps.collectionName}</h5>
+                    <ul key={`index${i}`} className="list-group">
+                        <li className="list-group-item" >TokenId: {i+1}</li>
+                        <li className="list-group-item">Name: {input.name} </li>
+                        <li className="list-group-item">Description: {input.description}</li>
+                        <li className="list-group-item">Price: {input.price} </li>
+                        <li className="list-group-item">Royalties: {input.royalties}</li>
+                    </ul>
+                </div>   
+        ))}
+    </div>
+ 
         </>
     )
 }
