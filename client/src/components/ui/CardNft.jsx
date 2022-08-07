531a0161244fe83533be74f26f475cfc39c660e3
GenesisBlock2140 <combnathan02@gmail.com>
2022-08-07 10:58:41 +0200

    Add Sell btn in CardNft for Profile page

--- client/src/components/ui/CardNft.jsx
+++ client/src/components/ui/CardNft.jsx
@@ -7,11 +7,16 @@ const CardNft = (props) => {
         navigate("/collections/" + props.goTo + "/" + props.nftId);
     };
 
+    const handleGoToSell = () => {
+        navigate("/sellnft/" + props.goTo + "/" + props.nftId);
+    };
+
     return(
         <div className="card--nft--background">
             <img src={props.nftImageUrl} className="card--nft--img"/>
             <p className="card--nft--id">#{props.nftId} {props.price ? "- " + props.price + " ETH": ""}</p>
             <button className="card--nft--btn" onClick={handleGoTo}>See more</button>
+            {props.isProfilePage && <button className="card--nft--btn--sell" onClick={handleGoToSell}>Sell</button>}
         </div>
     )
 }
